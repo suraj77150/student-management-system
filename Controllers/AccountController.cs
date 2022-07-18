6bd72f594b47449e1e3ba3482ee6173da79ed422
suraj yadav <78269810+suraj77150@users.noreply.github.com>
2022-07-17 22:27:11 -0700

    Add files via upload

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+using System.Web.Security;
+namespace WebApplication1.Controllers
+    
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Models.Membership model)
+
+        {
+            using(var context = new lindaaEntities())
+            {
+                bool isValid = context.User1.Any(x => x.Username == model.Username && x.Password == model.Password);
+                if(isValid)
+                {
+                    FormsAuthentication.SetAuthCookie(model.Username, false);
+                    return RedirectToAction("Index", "courses");
+                }
+                ModelState.AddModelError("", "Invalid username and password");
+            }
+
+            return View();
+        }
+
+        public ActionResult Signup()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Signup(User1 model)
+
+        {
+            using(var context = new lindaaEntities())
+            {
+                context.User1.Add(model);
+                context.SaveChanges();
+            }    
+            return RedirectToAction("Login");
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ Controllers/registerationsController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Authorize]
+    public class registerationsController : Controller
+    {
+        private lindaaEntities db = new lindaaEntities();
+
+        // GET: registerations
+        public ActionResult Index()
+        {
+            var registerations = db.registerations.Include(r => r.batch).Include(r => r.course);
+            return View(registerations.ToList());
+        }
+
+        // GET: registerations/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            registeration registeration = db.registerations.Find(id);
+            if (registeration == null)
+            {
+                return HttpNotFound();
+            }
+            return View(registeration);
+        }
+
+        // GET: registerations/Create
+        public ActionResult Create()
+        {
+            ViewBag.batch_id = new SelectList(db.batches, "id", "batch1");
+            ViewBag.course_id = new SelectList(db.courses, "id", "course1");
+            return View();
+        }
+
+        // POST: registerations/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,firstname,lastname,nic,course_id,batch_id")] registeration registeration)
+        {
+            if (ModelState.IsValid)
+            {
+                db.registerations.Add(registeration);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.batch_id = new SelectList(db.batches, "id", "batch1", registeration.batch_id);
+            ViewBag.course_id = new SelectList(db.courses, "id", "course1", registeration.course_id);
+            return View(registeration);
+        }
+
+        // GET: registerations/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            registeration registeration = db.registerations.Find(id);
+            if (registeration == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.batch_id = new SelectList(db.batches, "id", "batch1", registeration.batch_id);
+            ViewBag.course_id = new SelectList(db.courses, "id", "course1", registeration.course_id);
+            return View(registeration);
+        }
+
+        // POST: registerations/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,firstname,lastname,nic,course_id,batch_id")] registeration registeration)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(registeration).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.batch_id = new SelectList(db.batches, "id", "batch1", registeration.batch_id);
+            ViewBag.course_id = new SelectList(db.courses, "id", "course1", registeration.course_id);
+            return View(registeration);
+        }
+
+        // GET: registerations/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            registeration registeration = db.registerations.Find(id);
+            if (registeration == null)
+            {
+                return HttpNotFound();
+            }
+            return View(registeration);
+        }
+
+        // POST: registerations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            registeration registeration = db.registerations.Find(id);
+            db.registerations.Remove(registeration);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
